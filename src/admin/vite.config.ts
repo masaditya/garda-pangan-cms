31ce5a245a9f795ba4bb7b550c36734edc54274f
masaditya <aeka489@gmail.com>
2026-07-26 15:42:43 +0700

    main : fix admin ssl

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -4,6 +4,11 @@ export default (config: UserConfig) => {
   return mergeConfig(config, {
     server: {
       allowedHosts: ['cms.gardapangan.org'],
+      hmr: {
+        protocol: 'wss',
+        host: 'cms.gardapangan.org',
+        clientPort: 443,
+      },
     },
   });
 };
