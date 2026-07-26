044bf455871f0786a674222b7a0e9b116b806f5f
masaditya <aeka489@gmail.com>
2026-07-26 15:49:47 +0700

    main : fix admin server

--- config/server.js
+++ config/server.js
@@ -3,6 +3,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://cms.gardapangan.org'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
