2e9ca9323f0ea4da1aec0ee8f758246805e578fd
masaditya <aeka489@gmail.com>
2026-07-28 11:27:35 +0700

    main : delete seeder

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,5 @@
 export default {
   register(/* { strapi }: { strapi: Core.Strapi } */) {},
 
-  async bootstrap({ strapi }) {
-    // Seeder is disabled. Enable by uncommenting the block below and setting SEED_ON_BOOT=true.
-    // if (process.env.SEED_ON_BOOT !== 'true') {
-    //   return;
-    // }
-    //
-    // console.log('Starting Auto-Seeder from bootstrap (id + en)...');
-    // const { runSeed } = require('../scripts/run-seed-core');
-    // await runSeed(strapi);
-  },
+  async bootstrap({ strapi }) {},
 };
